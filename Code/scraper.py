55c6266c89e33ed274a59b798eece590ce11f8a8
javipmontes <javierpanimon@gmail.com>
2022-04-16 14:27:57 +0200

    scraper.py uploaded

--- Code/scraper.py
+++ Code/scraper.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
-url_dict = {}
-
-url = "https://www.investing.com/etfs/db-x-trackers-ii-global-sovereign-5"
+#Construct a dictionary with all the URLs that we have to access
+url_dict = {"stocks": "https://www.investing.com/funds/amundi-msci-wrld-ae-c-historical-data",
+            "corporate bonds": "https://www.investing.com/etfs/ishares-global-corporate-bond-$-historical-data", 
+            "public bonds": "https://www.investing.com/etfs/db-x-trackers-ii-global-sovereign-5-historical-data", 
+            "gold": "https://www.investing.com/etfs/spdr-gold-trust-historical-data", 
+            "cash": "https://www.investing.com/indices/usdollar-historical-data"}
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-
-driver.get(url)
