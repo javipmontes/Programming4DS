b64b4b0e2e765d1bb87fa30231328ac7851bba61
javipmontes <javierpanimon@gmail.com>
2022-04-17 21:50:38 +0200

    CSV format problem solved and got rid of Warning msgs

--- Code/ConnectToInvesting.py
+++ Code/ConnectToInvesting.py
@@ -3,11 +3,17 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 import time
 import os
+import pandas as pd
+import warnings
+
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+
+#Get rid of warning messages
+warnings.filterwarnings("ignore")
 
 # Create the directory where the CSVs are going to be stored; If it is already created, pass
 try:
@@ -34,7 +40,7 @@ class ConnectToInvesting:
         if connection_options is None:
             # Define the webdriver connection options
             connection_options = ['--headless', 'window-size=1920,1080', '--no-sandbox', '--disable-dev-shm-usage',
-                                  '--disable-notifications']
+                                  '--disable-notifications', 'log-level=3']
         # Store the name and URL of the asset
         self.asset_name = url.split('/')[-1]
         self.url = url+'-historical-data'
@@ -45,11 +51,11 @@ class ConnectToInvesting:
         for option in self.connection_options:
             self.webdriver_options.add_argument(option)
 
-    # Save the data in a csv file and store it inside the data folder 
-    def save_data(self, data):
+    # Save the dataframe in a csv file and store it inside the data folder 
+    def save_data(self, df):
         file_name = self.asset_name
         with open('data/{}.csv'.format(file_name), 'w') as f:
-            f.write(data)
+            df.to_csv('data/{}.csv'.format(file_name), sep = ";", index=False)
 
     # Stablish the connection and extract the required data
     def connection(self, start_date='01/01/2020', end_date='12/31/2020', save_file=True, print_data=False):
@@ -84,18 +90,41 @@ class ConnectToInvesting:
             print('Date range sent!')
             time.sleep(5)
 
-            # Retrieve desired data
+            # Retrieve the table data
             data = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'curr_table')))
+
+            row_list = []
+            #Access each row of the table
+            for row in data.find_elements_by_xpath('//*[@id="curr_table"]/tbody/tr'):
+                date = row.find_element_by_xpath('.//td[1]').text
+                price = row.find_element_by_xpath('.//td[2]').text
+                open = row.find_element_by_xpath('.//td[3]').text
+                high = row.find_element_by_xpath('.//td[4]').text
+                low = row.find_element_by_xpath('.//td[5]').text
+                change = row.find_element_by_xpath('.//td[6]').text
+                row_item = {
+                    "date": date,
+                    "price": price,
+                    "open": open,
+                    "high": high,
+                    "low": low,
+                    "change": change
+                }
+                row_list.append(row_item)
+
+            #Store the retrieved data in a pandas Dataframe
+            df = pd.DataFrame(row_list)
+
             print('Data received!')
 
             if save_file:
                 print('Saving data...')
-                self.save_data(data.text)
+                self.save_data(df)
                 print('Data saved!')
                 print('----------------')
 
             if print_data:
-                print(data.text)
+                print(df)
             driver.close()
 
 #URLs to be scraped
