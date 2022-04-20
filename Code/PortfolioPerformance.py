a49303fa65c07a9261d759ab709dc9fae2db29eb
Javier Pozo <javier.pozo@legit.health>
2022-04-20 16:40:16 +0200

    portfolio_metrics.csv uploaded

--- Code/PortfolioPerformance.py
+++ Code/PortfolioPerformance.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 def return_value():
-    go = pd.read_csv('data/spdr-gold-trust.csv')
-    ca = pd.read_csv('data/usdollar.csv')
-    st = pd.read_csv('data/amundi-msci-wrld-ae-c.csv')
-    cb = pd.read_csv('data/ishares-global-corporate-bond-$.csv')
-    pb = pd.read_csv('data/db-x-trackers-ii-global-sovereign-5.csv')
+    go = pd.read_csv('data/spdr-gold-trust.csv', sep=';')
+    ca = pd.read_csv('data/usdollar.csv', sep=';')
+    st = pd.read_csv('data/amundi-msci-wrld-ae-c.csv', sep=';')
+    cb = pd.read_csv('data/ishares-global-corporate-bond-$.csv', sep=';')
+    pb = pd.read_csv('data/db-x-trackers-ii-global-sovereign-5.csv', sep=';')
     p_al = pd.read_csv('data/portfolio_allocations.csv')
 
     #we store the prices of the assets at the the beginning of the year
@@ -35,13 +35,47 @@ def return_value():
     for i in range(len(p_al)):
         portfolio_return.append((current_value[i] - buy_amount[i]) / buy_amount[i] * 100)
 
+
     return portfolio_return
 
-    def volatility():
+def volatility():
+    go = pd.read_csv('data/spdr-gold-trust.csv', sep=';')
+    ca = pd.read_csv('data/usdollar.csv', sep=';')
+    st = pd.read_csv('data/amundi-msci-wrld-ae-c.csv', sep=';')
+    cb = pd.read_csv('data/ishares-global-corporate-bond-$.csv', sep=';')
+    pb = pd.read_csv('data/db-x-trackers-ii-global-sovereign-5.csv', sep=';')
+    p_al = pd.read_csv('data/portfolio_allocations.csv')
+    # print(p_al)
+    # print(go)
+
+    prices_paid = [st.iloc[-1]['Price'], cb.iloc[-1]['Price'], pb.iloc[-1]['Price'], go.iloc[-1]['Price'],
+                   ca.iloc[-1]['Price']]
+    # print(prices_paid)
+    p_al['shares_ST'] = p_al['ST'] * 100 / prices_paid[0]
+    p_al['shares_CB'] = p_al['CB'] * 100 / prices_paid[1]
+    p_al['shares_PB'] = p_al['PB'] * 100 / prices_paid[2]
+    p_al['shares_GO'] = p_al['GO'] * 100 / prices_paid[3]
+    p_al['shares_CA'] = p_al['CA'] * 100 / prices_paid[4]
+    # print(p_al)
+    assets_volatility = []
+    for asset in [go, ca, st, cb, pb]:
+        assets_volatility.append(100 * asset.Price.std() / asset.Price.mean())
+    # print(assets_volatility)
+    volatility = assets_volatility[0] * p_al['shares_ST'] + assets_volatility[1] * p_al['shares_CB'] + \
+                         assets_volatility[2] * p_al['shares_PB'] + assets_volatility[3] * p_al['shares_GO'] + \
+                         assets_volatility[4] * p_al['shares_CA']
+
+
+
+    return volatility
+
 
 
 if __name__ == '__main__':
 
-    portfolio_return = return_value()
     p_al = pd.read_csv('data/portfolio_allocations.csv')
-    p_al['RETURN'] = portfolio_return
+
+    p_al['RETURN'] = return_value()
+    p_al['VOLATILITY'] = volatility()
+
+    p_al.to_csv('data/portfolio_metrics.csv')
